cb0afff0e572347311f9142fec7b531d874caeaf
PabloH <pablohenriqq01@gmail.com>
2025-10-14 14:25:15 -0300

    Finished new scene lvl 5 and update flag script

--- Scripts/flag.gd
+++ Scripts/flag.gd
@@ -4,6 +4,8 @@ extends Area2D
 @export var level2 = "res://Scenes/level_2.tscn"
 @export var level3 = "res://Scenes/level_3.tscn"
 @export var level4 = "res://Scenes/level_4.tscn"
+@export var level5 = "res://Scenes/level_5.tscn"
+
 @export var level_final = "res://Scenes/level_final.tscn"
 
 func _on_area_entered(area: Area2D) -> void:
@@ -17,6 +19,8 @@ func _on_area_entered(area: Area2D) -> void:
 		elif current_scene_path == level3:
 			get_tree().change_scene_to_file(level4)
 		elif current_scene_path == level4:
+			get_tree().change_scene_to_file(level5)
+		elif current_scene_path == level5:
 			get_tree().change_scene_to_file(level_final)
 
 	pass # Replace with function body.
